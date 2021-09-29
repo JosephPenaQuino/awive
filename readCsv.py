aed267cb206557bc25f1035024eee4df8d8b0ffa
Joseph <joseph.pena@utec.edu.pe>
2021-09-29 04:22:15 -0300

    Plot vector in image

---
+++ readCsv.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+import matplotlib.cm
+import cv2 as cv
+from utils import ImageLoader
+
+path = ('/home/joseph/Documents/Thesis/Dataset/TowardHarmonization/dataset/'
+        'CastorRiver/20190410/Reference/ref5.csv')
+
+image_dataset = ('/home/joseph/Documents/Thesis/Dataset/TowardHarmonization/'
+                 'dataset/CastorRiver/20190410/Video3/Frames')
+image_len =  565
+image_offset = 51
+resize_ratio = 5
+
+def get_df():
+    df = pd.read_csv(path)
+    df = df.drop(['d'], axis=1)
+    df = df[df['v'].notna()]
+    df['v'] = df['v'].str.replace(',', '.').astype(float)
+    df['v'] = pd.to_numeric(df['v'])
+    return df
+
+
+def main():
+    df = get_df()
+    image_loader = ImageLoader(image_dataset, '', 5, image_offset)
+    im = image_loader.load(0)
+    im = cv.cvtColor(im, cv.COLOR_GRAY2RGB)
+    cmap = matplotlib.cm.get_cmap('Spectral')
+    minVal = df['v'].min()
+    maxVal = df['v'].max()
+    rangeVal = maxVal - minVal
+
+    # remove randomly
+    np.random.seed(10)
+    remove_n = 100
+    drop_indices = np.random.choice(df.index, remove_n, replace=False)
+    df = df.drop(drop_indices)
+
+    for i, val in df.iterrows():
+        v =(val['v']-minVal)/rangeVal
+        start_point = (int(val['x']), int(val['y']))
+        maxI = 400
+        end_point = (int(val['x']-maxI*v), int(val['y']-maxI*v))
+        rgb = cmap(v)
+        color = (255*rgb[0], 255*rgb[1], 255*rgb[2])
+        thickness = 20
+        im = cv.arrowedLine(im, start_point, end_point, color, thickness)
+
+    new_shape = (im.shape[0] // resize_ratio, im.shape[1] // resize_ratio)
+    im = cv.resize(im, new_shape)
+    cv.imshow('main', im)
+    cv.waitKey(0) 
+    cv.destroyAllWindows()
+        
+
+
+if __name__ == "__main__":
+    main()
