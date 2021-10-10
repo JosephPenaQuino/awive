1aa29edde37245d5fcb0a4e60cfe53e963d0a7bf
Joseph <joseph.pena@utec.edu.pe>
2021-10-09 21:30:40 -0300

    Add VideoLoader, Loader, Formatter, STIV classes

--- sti.py
+++ sti.py
@@ -3,8 +3,30 @@ import sys
 from matplotlib import pyplot as plt
 import numpy as np
 import json
-from utils import ImageLoader
+from utils import VideoLoader
+from utils import Formatter
 
+
+class STIV():
+    def __init__(self, shape, frames_qnt, ref):
+        self._frames_qnt = frames_qnt
+        self._ref = ref
+        self._width = shape[0]
+        self._height = shape[1]
+        self._stis = [np.empty((1, self._height), dtype=np.uint8) for i in
+                range(frames_qnt)]
+
+
+    def append(self, image):
+        for i in range(self._frames_qnt):
+            offset = i*10
+            ref = self._width//2 + offset
+            row = image[ref:ref+ 1, :]
+            self._stis[i] = np.vstack([self._stis[i], row])
+
+    @property
+    def stis(self, idx):
+        return self._stis[idx]
 
 def adjust_gamma(image, gamma=1.0):
     # build a lookup table mapping the pixel values [0, 255] to
@@ -18,13 +40,7 @@ def adjust_gamma(image, gamma=1.0):
 def main(option):
     # Read configuration
     conf = json.load(open('conf/sti.json'))[option]
-    video_path = conf['video_path']
-
-    # Read video
-    cap = cv.VideoCapture(video_path)
-    ret, frame = cap.read()
-    width = frame.shape[0]
-    height = frame.shape[1]
+    loader = VideoLoader(conf['video_path'], conf['offset'])
     h1 = conf['roi']['h1']
     h2 = conf['roi']['h2']
     w1 = conf['roi']['w1']
@@ -32,72 +48,56 @@ def main(option):
     w = w2 - w1
     h = h2 - h1
 
-    frameNumber = 1
+    # Set formatter
+    im = loader.read()
+    formatter = Formatter(im.shape,
+                         -25,
+                         1.0,
+                         conf['roi']['w1'],
+                         conf['roi']['w2'],
+                         conf['roi']['h1'],
+                         conf['roi']['h2'])
 
-    sti = [np.empty((1, h), dtype=np.uint8) for i in range(frameNumber)]
-    clahe = cv.createCLAHE(clipLimit=3.0, tileGridSize=(4, 4))
-
-
-    for i in range(55):
-        ret, frame = cap.read()
 
     # Get average 
     imMean =  np.zeros((w, h), dtype=np.int64)
     cnt = 0
-    while cap.isOpened():
-        ret, imm = cap.read()
-        if not ret:
-            break
-        M = cv.getRotationMatrix2D((width//2, height//2), -25, 1.0)
-        imm = cv.warpAffine(imm, M, (width, height))
-        imm = imm[w1:w2, h1:h2]
-        imm = cv.cvtColor(imm, cv.COLOR_BGR2GRAY)
-        imm = imm.astype(int)
-        imMean += imm
+    while loader.has_images():
+        im = formatter.apply(loader.read()).astype(int)
+        imMean += im
         cnt += 1
-
-    print('cnt:', cnt)
-    print('imMean:', np.max(imMean), np.min(imMean))
     imMean = (imMean / cnt) * 1
     imMean = imMean.astype(np.uint8)
-    print('imMean:', np.max(imMean), np.min(imMean))
     cv.imshow('image mean', imMean)
+    loader.finish()
 
-    cap = cv.VideoCapture(video_path)
-    for i in range(55):
-        ret, frame = cap.read()
+    # Apply STIV
+    loader = VideoLoader(conf['video_path'], conf['offset'])
+    frameNumber = 1
+    ref = 9
+    stiv = STIV((w, h), frameNumber, ref)
 
+    # clahe = cv.createCLAHE(clipLimit=3.0, tileGridSize=(4, 4))
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-        imm = frame
-        M = cv.getRotationMatrix2D((width//2, height//2), -25, 1.0)
-        imm = cv.warpAffine(imm, M, (width, height))
-        imm = imm[w1:w2, h1:h2]
-        imm = cv.cvtColor(imm, cv.COLOR_BGR2GRAY)
+    while loader.has_images():
+        im = formatter.apply(loader.read())
         # imm -= imMean
         # imm = clahe.apply(imm)
         # imm = (255.0*(((imm - 80).astype("uint8")) / 120)).astype("uint8")
         # imm = adjust_gamma(imm, 0.9)
         # np.save("tmp2.npy", imm)
-        for i in range(frameNumber):
-            offset = i*10
-            ref = w//2 + offset
-            row = imm[ref:ref+ 1, :]
-            sti[i] = np.vstack([sti[i], row])
+        stiv.append(im)
     
     for i in range(frameNumber):
-        cv.imshow(f'sti_{i:02}', sti[i])
-    cv.imshow("frame", imm)
+        cv.imshow(f'sti_{i:02}', stiv._stis[i])
+    cv.imshow("frame", im)
     cv.waitKey(0) 
-    cap.release()
     cv.destroyAllWindows()
+    loader.finish()
 
 
 if __name__ == '__main__':
-    option = 'd1'
+    option = 'd0'
     if len(sys.argv) > 1:
         option = sys.argv[1]
     main(option)
