97848155e2222eef7f56bbbedecfbdc80c9654ef
Joseph <joseph.pena@utec.edu.pe>
2021-11-15 21:39:40 -0300

    feat: add options u and r in analyze_image and video files

--- analyze_image.py
+++ analyze_image.py
@@ -10,14 +10,18 @@ from correct_image import Formatter
 FOLDER_PATH = '/home/joseph/Documents/Thesis/Dataset/config'
 
 
-def main(config_path: str, video_identifier: str, entire_frame=False):
+def main(config_path: str, video_identifier: str, entire_frame=False,
+        undistort=True, roi=True):
     '''Save the first image as numpy file'''
     loader = get_loader(config_path, video_identifier)
     formatter = Formatter(config_path, video_identifier)
     image = loader.read()
     if entire_frame:
         formatter.show_entire_image()
-    image = formatter.apply_roi_extraction(image)
+    if undistort:
+        image = formatter.apply_distortion_correction(image)
+    if roi:
+        image = formatter.apply_roi_extraction(image)
     np.save('tmp.npy', image)
 
 
@@ -34,8 +38,20 @@ if __name__ == "__main__":
         '--frame',
         action='store_true',
         help='Plot entire frame or not')
+    parser.add_argument(
+        '-u',
+        '--undistort',
+        action='store_true',
+        help='Format image using distortion correction')
+    parser.add_argument(
+        '-r',
+        '--roi',
+        action='store_true',
+        help='Format image using selecting only roi area')
     args = parser.parse_args()
     CONFIG_PATH = f'{FOLDER_PATH}/{args.statio_name}.json'
     main(config_path=CONFIG_PATH,
          video_identifier=args.video_identifier,
-         entire_frame=args.frame)
+         entire_frame=args.frame,
+         undistort=args.undistort,
+         roi=args.roi)
--- video.py
+++ video.py
@@ -4,17 +4,22 @@ import argparse
 import cv2
 
 from loader import get_loader, Loader
+from correct_image import Formatter
 
 
 FOLDER_PATH = '/home/joseph/Documents/Thesis/Dataset/config'
 RESIZE_RATIO = 5
 
 
-def play(loader: Loader):
+def play(loader: Loader, formatter: Formatter, undistort=True, roi=True):
     '''Plays a video'''
 
     while loader.has_images():
         image = loader.read()
+        if undistort:
+            image = formatter.apply_distortion_correction(image)
+        if roi:
+            image = formatter.apply_roi_extraction(image)
         lil_im = cv2.resize(image, (1000, 1000))
         cv2.imshow('Video', lil_im)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -23,10 +28,11 @@ def play(loader: Loader):
     cv2.destroyAllWindows()
 
 
-def main(config_path: str, video_identifier: str):
+def main(config_path: str, video_identifier: str, undistort=True, roi=True):
     '''Read configurations and play video'''
     loader = get_loader(config_path, video_identifier)
-    play(loader)
+    formatter = Formatter(config_path, video_identifier)
+    play(loader, formatter, undistort, roi)
 
 
 if __name__ == "__main__":
@@ -37,6 +43,20 @@ if __name__ == "__main__":
     parser.add_argument(
         "video_identifier",
         help="Index of the video of the json config file")
+    parser.add_argument(
+        '-u',
+        '--undistort',
+        action='store_true',
+        help='Format image using distortion correction')
+    parser.add_argument(
+        '-r',
+        '--roi',
+        action='store_true',
+        help='Format image using selecting only roi area')
     args = parser.parse_args()
     CONFIG_PATH = f'{FOLDER_PATH}/{args.statio_name}.json'
-    main(config_path=CONFIG_PATH, video_identifier=args.video_identifier)
+    main(config_path=CONFIG_PATH,
+         video_identifier=args.video_identifier,
+         undistort=args.undistort,
+         roi=args.roi
+         )
