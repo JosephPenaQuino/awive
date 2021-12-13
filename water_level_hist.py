ea1cf090f6067b5b0bdaab11174480ee12b584e7
Joseph <joseph.pena@utec.edu.pe>
2021-12-13 03:42:57 -0300

    feat: water_level_hist: change funcionity to process other region of the river

--- water_level_hist.py
+++ water_level_hist.py
@@ -1,3 +1,4 @@
+#!/home/joseph/anaconda3/envs/imageProcessing/bin/python3
 '''
 Water Level Detector using method described in:
 Embedded implementation of image-based water-level measurement system
@@ -25,6 +26,9 @@ class WaterlevelDetector:
         self._loader = get_loader(config_path, video_identifier)
         self._buffer_length = config['buffer_length']
         roi  = config['roi']
+        roi2  = config['roi2']
+        self._wr0 = slice(roi2[0][0], roi2[1][0])
+        self._wr1 = slice(roi2[0][1], roi2[1][1])
         self._r0 = slice(roi[0][0], roi[1][0])
         self._r1 = slice(roi[0][1], roi[1][1])
         self._roi_shape = (roi[1][0] - roi[0][0], roi[1][1] - roi[0][1])
@@ -35,28 +39,39 @@ class WaterlevelDetector:
     def _get_difference_accumulation(self):
         cnt = 0
         buffer = []
+        accumulated_image = np.zeros(self._roi_shape)
 
-        for _ in range(self._buffer_length):
+        image = self._loader.read()
+        ref_image = image[self._wr0, self._wr1]
+        np.save('im_ref.npy', ref_image)
+        image = image[self._r0, self._r1]
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        for i in range(self._buffer_length):
             if not self._loader.has_images():
                 print('broke')
                 return None
-            image = self._loader.read()[self._r0, self._r1]
-            cv2.imwrite('im_color.jpg', image)
-            buffer.append(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
-            cnt +=1
-        accumulated_image = np.zeros(self._roi_shape)
+            new_image = self._loader.read()[self._r0, self._r1]
+            new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2GRAY)
 
-        for image in buffer[:-1]:
-            new_difference = abs(buffer[-1] - image)
-            np.save('i0.npy', buffer[-1])
-            np.save('i1.npy', image)
-            np.save('i2.npy', new_difference)
-            accumulated_image += new_difference
-        accumulated_image = np.interp(
-                accumulated_image,
-                (accumulated_image.min(), accumulated_image.max()),
-                (0, 255)
-                ).astype(np.uint8)
+            accumulated_image += (new_image - image) ** 2
+            image = new_image
+            # buffer.append(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
+            # cnt +=1
+
+        # for image in buffer[:-1]:
+            # new_difference = abs(buffer[-1] - image)
+            # np.save('i0.npy', buffer[-1])
+            # np.save('i1.npy', image)
+            # np.save('i2.npy', new_difference)
+            # accumulated_image += new_difference
+        # accumulated_image = np.interp(
+            #     accumulated_image,
+            #     (accumulated_image.min(), accumulated_image.max()),
+            #     (0, 255)
+            #     ).astype(np.uint8)
+        np.save('acc.npy', accumulated_image)
+        print('average:', round(accumulated_image.mean(), 2))
 
         return accumulated_image
 
@@ -97,17 +112,18 @@ class WaterlevelDetector:
         '''calculate and return water level'''
         accumulated_image = self._get_difference_accumulation()
         np.save('out0.npy', accumulated_image)
-        threshold, _ = self._get_threshold(accumulated_image)
-        height = self._compute_water_level(accumulated_image, threshold)
+        # threshold, _ = self._get_threshold(accumulated_image)
+        # height = self._compute_water_level(accumulated_image, threshold)
+        return 0
 
-        return height
+        # return height
 
 
 def main(config_path: str, video_identifier: str, show_image=True):
     '''Execute basic example of water level detector'''
     water_level_detector = WaterlevelDetector(config_path, video_identifier)
     water_level = water_level_detector.get_water_level()
-    print('water level:', water_level)
+    # print('water level:', water_level)
 
 
 if __name__ == '__main__':
