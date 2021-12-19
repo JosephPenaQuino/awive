b670644ca0644180bdaf8cbc985d2e6fe561a30c
Joseph <joseph.pena@utec.edu.pe>
2021-12-18 23:41:24 -0300

    fix: main: syntaz error

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ import water_level_hist
 FOLDER_PATH = '/home/joseph/Documents/Thesis/Dataset/config'
 
 def execute_method(method_name, station_name, video_identifier, config_path):
+    print(method_name)
     if method_name == 'sti':
         ret = sti.main(config_path, video_identifier)
     elif method_name == 'otv':
         ret = otv.main(config_path, video_identifier)
     else:
         ret = None
-    print(method_name)
     for i in range(len(ret)):
         print(ret[str(i)]['velocity'])
 
@@ -25,7 +25,7 @@ def execute_method(method_name, station_name, video_identifier, config_path):
 def main(station_name, video_identifier, config_path, th):
     '''execute one method methods'''
     idpp = water_level_hist.main(config_path, video_identifier)
-    if idpp < th:
+    if idpp > th:
         execute_method('sti', station_name, video_identifier, config_path)
     else:
         execute_method('otv', station_name, video_identifier, config_path)
@@ -55,6 +55,6 @@ if __name__ == "__main__":
     main(
         station_name=args.station_name,
         video_identifier=args.video_identifier,
-        config_path=CONFIG_PATH
+        config_path=CONFIG_PATH,
         th=args.th
         )
