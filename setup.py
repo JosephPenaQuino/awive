ff7e6a57a8e8b0d6d257fe0ee5440243860f73e1
Joseph P <joseph.pena@utec.edu.pe>
2022-11-03 20:59:44 -0300

    build: add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='adaptive-water-image-velocimetr-estimator',
+    version='0.1dev',
+    packages=['awive',],
+    license='',
+    long_description=open('README').read(),
+)
