a6315f722a1954b9fc7913e7b63e9c04963205d9
Joseph P <joseph.pena@utec.edu.pe>
2022-11-19 16:06:15 -0300

    feat: add config

--- awive/config.py
+++ awive/config.py
@@ -3,6 +3,56 @@
 from pydantic import BaseModel
 
 
+class Position(BaseModel):
+    """Pixels or meters."""
+
+    x: list[int]
+    y: list[int]
+
+
+class GroundTruth(BaseModel):
+    """Ground truth data."""
+
+    position: list[int]
+    velocity: float
+
+
+class ConfigGcp(BaseModel):
+    """Configurations GCP."""
+
+    apply: bool
+    pixels: Position
+    meters: Position
+    ground_truth: GroundTruth
+
+
+class ConfigRoi(BaseModel):
+    """Configurations ROI."""
+
+    h1: int
+    h2: int
+    w1: int
+    w2: int
+
+
+class ConfigImageCorrection(BaseModel):
+    """Configuration Image Correction."""
+
+    apply: bool
+    k1: float
+    c: float
+    f: float
+
+
+class ConfigPreProcessing(BaseModel):
+    """Configurations pre-processing."""
+
+    rotate_image: int
+    pre_roi: ConfigRoi
+    roi: ConfigRoi
+    image_correction: ConfigImageCorrection
+
+
 class ConfigDataset(BaseModel):
     """Configuration dataset."""
 
@@ -13,14 +63,64 @@ class ConfigDataset(BaseModel):
     video_path: str
     width: int
     height: int
+    ppm: int
+    gcp: ConfigGcp
+
+
+class ConfigOtvFeatures(BaseModel):
+    """Config for OTV Features."""
+
+    maxcorner: int
+    qualitylevel: float
+    mindistance: int
+    blocksize: int
+
+
+class ConfigOtvLucasKanade(BaseModel):
+    """Config for OTV Lucas Kanade."""
+
+    winsize: int
+    max_level: int
+    max_count: int
+    epsilon: float
+    flags: int
+    radius: int
+    min_eigen_threshold: float
 
 
 class ConfigOtv(BaseModel):
     """Configuration OTV."""
 
+    mask_path: str
+    pixel_to_real: float
+    partial_min_angle: float
+    partial_max_angle: float
+    final_min_angle: float
+    final_max_angle: float
+    max_Features: int
+    region_step: int
+    resolution: int
+    features: ConfigOtvFeatures
+    lk: ConfigOtvLucasKanade
+    lines: list[int]
+
+
+class ConfigStivLine(BaseModel):
+    """Config for STIV line."""
+
+    start: list[int]
+    end: list[int]
+
 
 class ConfigStiv(BaseModel):
     """Configuration STIV."""
+
+    window_shape: list[int]
+    filter_window: int
+    overlap: int
+    ksize: int
+    polar_filter_width: int
+    lines: list[ConfigStivLine]
 
 
 class Config(BaseModel):
