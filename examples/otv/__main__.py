b8104525c3995300d9c920a10df8c5d88de32a37
Joseph P <joseph.pena@utec.edu.pe>
2022-11-19 02:05:46 -0300

    example: add empty otv example

---
+++ examples/otv/__main__.py
@@ -0,0 +1,34 @@
+"""Basic example of using OTV."""
+
+from pathlib import Path
+
+import time
+import json
+
+from awive.config import Config
+from awive.loader import Loader, make_loader
+from awive.algorithms.otv import Otv
+
+
+CONFIG_PATH = "examples/basic/config.json"
+VIDEO_ID = "basic"
+
+
+def run_otv(config_path: str, video_identifier: str) -> None:
+    """Run OTV on a dataset."""
+    config: Config = Config(
+        **json.loads(
+            Path(config_path).read_text()
+        )[video_identifier]
+    )
+
+    loader: Loader = make_loader(config.dataset)
+    otv: Otv = Otv(config.otv)
+    start: float = time.time()
+    otv.run(loader)
+    end: float = time.time()
+    print(f"OTV took {end - start} seconds")
+
+
+if __name__ == "__main__":
+    run_otv(CONFIG_PATH, VIDEO_ID)
